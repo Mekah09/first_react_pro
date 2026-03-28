6a2b70c504fd97413ef263da1dced8e583b361d8
Abdulhakeem Zakariyau <zakariyauabdulhakeem32@gmail.com>
2026-03-28 07:21:24 +0100

    first commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+import "../css/Favorites.css"
+function Favorite(){
+  return <div className="favourites-empty">
+    <h2>No favourites Movies Yet</h2>
+    <p>Start adding monies to your favourites and they will appear here</p>
+  </div>
+}
+
+export default Favorite
---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import Moviecard from "../components/MovieCard"
+import {useState} from "react"
+import "../css/Home.css"
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "John wick", release_date: "2020" },
+    { id: 2, title: "Near", release_date: "2021" },
+    { id: 3, title: "Away", release_date: "2022" },
+    { id: 4, title: "Ride or die", release_date: "2023" },
+  ]
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    alert(searchQuery)
+    setSearchQuery("-----")
+  }
+
+  return <div className="home">
+    <form onSubmit={handleSearch} className="search-form">
+      <input 
+      type="text"
+      placeholder="search for movie..." 
+      className="search-input"
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button type="submit" className="search-button">Search</button>
+    </form>
+
+    <div className="movies-grid">
+      {movies.map(
+        (movie) => 
+        // movie.title.toLowerCase().startsWith(searchQuery) && 
+        (
+          <Moviecard movie={movie} key={movie.id} />
+          ) 
+        )}
+    </div>
+  </div>
+}
+
+export default Home
